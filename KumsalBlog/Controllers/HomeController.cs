126e36fb38b337407b0f9b9171266409b1ab6294
ahsalmonskinroll <kumsal.celebiii@gmail.com>
2024-05-18 01:08:37 +0300

    17.05.24 - Bütün sayfalar yüklendi. Backend ve veri tabanı bağlantıları yapılacak.

--- KumsalBlog/Controllers/HomeController.cs
+++ KumsalBlog/Controllers/HomeController.cs
@@ -18,12 +18,52 @@ namespace KumsalBlog.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult YazilimMuhendisligi()
         {
             return View();
         }
 
-        public IActionResult About()
+		public IActionResult YazilimMuhDetay()
+		{
+			return View();
+		}
+
+		public IActionResult DKUG()
+		{
+			return View();
+		}
+
+		public IActionResult DKUGDetay()
+		{
+			return View();
+		}
+
+		public IActionResult Bitirme()
+		{
+			return View();
+		}
+
+		public IActionResult BitirmeDetay()
+		{
+			return View();
+		}
+
+		public IActionResult Viki()
+		{
+			return View();
+		}
+
+		public IActionResult VikiDetay()
+		{
+			return View();
+		}
+
+		public IActionResult About()
         {
             return View();
         }
@@ -38,8 +78,42 @@ namespace KumsalBlog.Controllers
             return View();
         }
 
+		public IActionResult BlogAralik()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult BlogTenis()
+		{
+			return View();
+		}
+
+		public IActionResult BlogTubitak()
+		{
+			return View();
+		}
+
+		public IActionResult BlogSeramik()
+		{
+			return View();
+		}
+
+		public IActionResult BlogStaj()
+		{
+			return View();
+		}
+
+		public IActionResult BlogKayak()
+		{
+			return View();
+		}
+
+		public IActionResult BlogBuzHokeyi()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
